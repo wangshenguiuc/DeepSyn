685156ddf703773b4960cc3a88cef1ded0642844
Jianzhu Ma <majianzhu@deepsyn.ucsd.edu>
2020-01-29 20:32:10 +0000

    readme

--- run_deepsyn_fast.py
+++ run_deepsyn_fast.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from utils import *
-DATA_DIR = '/home/majianzhu/DeepSyn/data/knowledge_network/'
+DATA_DIR = 'DeepSyn/data/knowledge_network/'
 print (DATA_DIR)
 diffusion, diffusion_n2i, diffusion_i2n, networks, node2tp, tp2node, node2ct = read_server_data(DATA_DIR)
 
